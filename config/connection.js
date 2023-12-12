bfccd0853b3be51270cbd68ca32702744df623d3
cwoolcott <christopher.woolcott@gmail.com>
2023-12-11 21:55:46 -0600

    test

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,18 @@
 const Sequelize = require("sequelize");
 require("dotenv").config();
-const sequelize = new Sequelize(
-  "postgres://bobs_cars_user:owdmWvgqTv3vOxV9aTHYTaVGwud9owGY@dpg-clrsvffqd2ns73dt0ub0-a.ohio-postgres.render.com/bobs_cars?ssl=true"
-);
-//?ssl=true
-
-// const sequelize = new Sequelize(
-//   process.env.DB_NAME,
-//   process.env.DB_USER,
-//   process.env.DB_PASSWORD,
-//   {
-//     host: "dpg-clrsvffqd2ns73dt0ub0-a",
-//     dialect: "postgres",
-//     port: 5432,
-//   }
-// );
-
+if (process.env.POSTGRES_DB) {
+  const sequelize = new Sequelize(process.env.POSTGRES_DB);
+  //?ssl=true
+} else {
+  const sequelize = new Sequelize(
+    process.env.DB_NAME,
+    process.env.DB_USER,
+    process.env.DB_PASSWORD,
+    {
+      host: "localhost",
+      dialect: "mysql",
+      port: 3306,
+    }
+  );
+}
 module.exports = sequelize;
